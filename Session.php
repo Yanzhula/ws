8206f50b1033a37b677dd494346bbcfd8810e32f
yan <yan@yan-hp>
2013-06-03 17:03:21 +0300

    updates

---
+++ Session.php
@@ -0,0 +1,22 @@
+<?php
+namespace ws;
+
+class Session {
+    const SESSION_NAMESPACE = 'WS7';
+    protected $data;
+
+    public function __construct() {
+        if (!session_id()) session_start();
+        $this->data = &$_SESSION[self::SESSION_NAMESPACE];
+    }
+
+    public function __set($name, $value){
+        $this->data[$name] = $value;
+    }
+
+    public function __get($name){
+        if (array_key_exists($name, $this->data)) return $this->data[$name];
+        return null;
+    }
+}
+?>
